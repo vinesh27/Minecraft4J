96dbee84d3f6fc3d4aeb23efd9c2a88f0e424fac
vinesh27 <vinitrajpurohit00@gmail.com>
2022-04-11 19:25:28 +0530

    First Endpoint: Get UUID Done!

---
+++ src/main/java/io/github/vinesh27/minecraf4j/Example.java
@@ -0,0 +1,11 @@
+package io.github.vinesh27.minecraf4j;
+
+import java.util.ArrayList;
+
+public class Example {
+    public static void main(String[] args) {
+        Minecraft minecraft = new Minecraft();
+        String uuid = minecraft.getUUID("name");
+        
+    }
+}
--- src/main/java/io/github/vinesh27/minecraf4j/Minecraft.java
+++ src/main/java/io/github/vinesh27/minecraf4j/Minecraft.java
@@ -1,5 +1,56 @@
 package io.github.vinesh27.minecraf4j;
 
-public class Minecraft {
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
 
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.util.ArrayList;
+
+public class Minecraft {
+    private HttpClient client;
+    
+    public Minecraft() {
+        client = HttpClient.newBuilder()
+            .version(HttpClient.Version.HTTP_2)
+            .build();
+    }
+    
+    public Minecraft(HttpClient client) { this.client = client; }
+    
+    public HttpClient getClient() { return client; }
+    public void setClient(HttpClient client) { this.client = client; }
+    
+    //https://wiki.vg/Mojang_API#Username_to_UUID
+    /**
+     * Gets the UUID of a player from their name
+     * @param name - The name of the player
+     * @return String - The UUID of the player
+     */
+    public String getUUID(String name)  {
+        HttpRequest request = HttpRequest.newBuilder()
+            .uri(URI.create("https://api.mojang.com/users/profiles/minecraft/" + name))
+            .GET()
+            .build();
+        HttpResponse<String> response;
+        try {
+            response = client.send(request, HttpResponse.BodyHandlers.ofString());
+            if (response.statusCode() == 204) return null;
+            if (response.statusCode() == 200) {
+                try {
+                    return ((JSONObject) new JSONParser().parse(response.body())).get("id").toString();
+                } catch (ParseException e) {
+                    return "Parse ERROR [" + response.statusCode() + "]: " + response.body();
+                }
+            }
+        } catch (IOException | InterruptedException e) {
+            e.printStackTrace();
+        }
+        return "ERROR";
+    }
+    
 }
